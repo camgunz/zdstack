d89a84e4c40f3c81340889a5e06aded784bfb104
Charles.Gunyon <Charles.Gunyon@36f78971-4549-0410-b7da-6d57fc7fc606>
2008-04-18 06:22:10 +0000

    Added setup script
    git-svn-id: http://zdstack.googlecode.com/svn@89 36f78971-4549-0410-b7da-6d57fc7fc606

---
+++ trunk/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ZDStack',
+      version='0.1',
+      description='ZDStack zserv ZDaemon Server Wrapper',
+      author='Charlie Gunyon',
+      author_email='charles.gunyon@gmail.com',
+      url='http://zdstack.googlecode.com/',
+      scripts=['bin/zdstackctl'],
+      packages=['ZDStack'],
+     )
