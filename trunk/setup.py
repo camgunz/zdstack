13059afb46ed0c568a3c42432226176a8b44a4d3
Charles.Gunyon <Charles.Gunyon@36f78971-4549-0410-b7da-6d57fc7fc606>
2008-04-24 08:33:28 +0000

    Added zservctl script, disabled logging because it was horribly abused during debugging.  Pending testing, this will be 1.0.
    git-svn-id: http://zdstack.googlecode.com/svn@103 36f78971-4549-0410-b7da-6d57fc7fc606

--- trunk/setup.py
+++ trunk/setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='ZDStack',
-      version='0.1',
+      version='0.9',
       description='ZDStack zserv ZDaemon Server Wrapper',
       author='Charlie Gunyon',
       author_email='charles.gunyon@gmail.com',
       url='http://zdstack.googlecode.com/',
-      scripts=['bin/zdstackctl'],
+      scripts=['bin/zdstackctl', 'bin/zservctl'],
       packages=['ZDStack'],
      )
