5d67f1518b8cb23e162b1e4dca9192d04fd434cc
Charles.Gunyon <Charles.Gunyon@36f78971-4549-0410-b7da-6d57fc7fc606>
2008-07-24 18:24:51 +0000

    Added fair_teams plugin
    git-svn-id: http://zdstack.googlecode.com/svn@189 36f78971-4549-0410-b7da-6d57fc7fc606

--- plugins/fair_teams.py
+++ plugins/fair_teams.py
@@ -1,3 +1,10 @@
+from threading import Timer
+
+MIN_PLAYERS = 0
+MAX_PLAYERS = 16
+
+BALANCE_WINDOW = 15
+
 def fair_teams(event, zserv):
     if not event.type in ('team_join', 'team_switch'):
         return
@@ -12,6 +19,30 @@ def fair_teams(event, zserv):
         spec_colors = ('white')
     elif max_teams = 4:
         colors = ('red', 'blue', 'green', 'white')
+    def _teams_are_unbalanced():
+        d = {}
+        ps = zserv.players.values()
+        for c in colors:
+            tp = [x for x in ps if x.color == c and x.playing]
+            ps = [x for x in ps if x not in tp]
+            d[t.color] = len(tp)
+        average = sum(d.values()) / len(colors)
+        if average == MAX_PLAYERS:
+            upper_bound = MAX_PLAYERS
+        else:
+            upper_bound = average + 1
+        if average == MIN_PLAYERS:
+            lower_bound = MIN_PLAYERS
+        else:
+            lower_bound = average - 1
+        above_average = [x for x in d if d[x] >= upper_bound]
+        below_average = [x for x in d if d[x] <= lower_bound]
+        if not (above_average and below_average):
+            return False
+        return (above_average, below_average)
+    def _kick_unbalancer(p):
+        if _teams_are_unbalanced():
+            zserv.kick(p.number, "Keep the teams balanced")
     if event.type == 'team_join':
         losing_teams = []
         for team in [y for x, y in zserv.teams.items() if x in colors]:
@@ -49,14 +80,19 @@ def fair_teams(event, zserv):
             reason += "or if equal, the team that is losing"
             zserv.kick(player.number, reason)
     elif event.type == 'team_switch':
-        d = dict([(x, len([y for y in zserv.players.values() if y.color == x])) \
-                      for x in zserv.teams.values()])
-        d = {}
-        for c in colors:
-            tp = [x for x in zserv.players.values() if x.color == c]
-            d[t.color] = len(tp)
-        average = sum(d.values()) / len(colors)
-        above_average = [x for x in d if d[x] > average]
-        below_average = [x for x in d if d[x] <= average]
-        if event.data['team'] in above_average:
-            
+        x = _teams_are_unbalanced()
+        if not x:
+            return
+        above_average, below_average = x
+        if event.data['team'] not in above_average:
+            return
+        if len(above_average) == 3:
+            msg1 = "The %s, %s and %s teams have too many players"
+        elif len(above_average) == 2:
+            msg1 = "The %s and %s teams have too many players"
+        else:
+            msg1 = "The %s team has too many players"
+        msg2 = ", even the teams or %s will be kicked" % (player.name)
+        zserv.zsay(msg1 + msg2 % (above_average))
+        Timer(BALANCE_WINDOW, _kick_unbalancer, args=[player]).start()
+
