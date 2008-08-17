0bf99d9284f81aeda3760367aee3c25dc703cf43
Charles.Gunyon <Charles.Gunyon@36f78971-4549-0410-b7da-6d57fc7fc606>
2008-08-17 23:18:18 +0000

    Added CHANGES file, and make_release.sh script for easy packaging
    git-svn-id: http://zdstack.googlecode.com/svn@207 36f78971-4549-0410-b7da-6d57fc7fc606

---
+++ trunk/make_release.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+if [ ! $1 ]; then
+  echo
+  echo "Usage: `basename $0` [ version ]"
+  echo
+  exit -1
+fi
+
+RELEASE_DIR="zdstack-$1"
+
+rm -f $RELEASE_DIR.tar.bz2
+chmod 777 $RELEASE_DIR -R 2> /dev/null; rm -rf $RELEASE_DIR
+mkdir -p $RELEASE_DIR/bin $RELEASE_DIR/doc/pydocs $RELEASE_DIR/ZDStack
+
+cp bin/zdstack bin/zservctl $RELEASE_DIR/bin/
+cp doc/pydocs/*.html  $RELEASE_DIR/doc/pydocs/
+cp doc/zdstack.ini-example $RELEASE_DIR/doc/
+cp ZDStack/*.py $RELEASE_DIR/ZDStack/
+cp * $RELEASE_DIR/ 2> /dev/null
+rm -f $RELEASE_DIR/make_release.sh
+
+chmod 640 $RELEASE_DIR/doc/pydocs/*.html
+chmod 640 $RELEASE_DIR/doc/zdstack.ini-example
+chmod 640 $RELEASE_DIR/ZDStack/*.py
+chmod 640 $RELEASE_DIR/CHANGES
+chmod 640 $RELEASE_DIR/INSTALL
+chmod 640 $RELEASE_DIR/LICENSE
+chmod 640 $RELEASE_DIR/README
+chmod 750 $RELEASE_DIR/bin
+chmod 750 $RELEASE_DIR/doc
+chmod 750 $RELEASE_DIR/doc/pydocs
+chmod 750 $RELEASE_DIR/ZDStack
+chmod 750 $RELEASE_DIR/bin/* $RELEASE_DIR/setup.py
+
+tar cjf $RELEASE_DIR.tar.bz2 $RELEASE_DIR
+chmod 777 $RELEASE_DIR -R 2> /dev/null; rm -rf $RELEASE_DIR
+
