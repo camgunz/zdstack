8c1df30c2b7da10225f47aa6e2c2a2d3e25f3639
Charles.Gunyon <Charles.Gunyon@36f78971-4549-0410-b7da-6d57fc7fc606>
2008-09-04 05:44:29 +0000

    Updated clean_language
    git-svn-id: http://zdstack.googlecode.com/svn@222 36f78971-4549-0410-b7da-6d57fc7fc606

--- plugins/clean_language.py
+++ plugins/clean_language.py
@@ -11,16 +11,18 @@ BAD_LANGUAGE_LIMIT = 2
 def clean_language(event, zserv):
     if not event.type == 'message':
         return
-    p = zserv.distill_player(event.data['possible_player_names'])
-    if not p: # oddly, the messenger could not be distilled
-        return
+    message, messenger = (event.data['message'], event.data['messenger'])
     for w in BAD_WORDS:
-        if w in event.data['contents']:
-            if not hasattr(p, 'bad_language'):
-                p.bad_language = 0
-            p.bad_language += 1
-            if p.bad_language >= BAD_LANGUAGE_LIMIT:
-                p.bad_language = 0
-                zserv.zkick(p.number)
+        if w in message:
+            if not hasattr(messenger, 'bad_language'):
+                messenger.bad_language = 0
+            messenger.bad_language += 1
+            if messenger.bad_language >= BAD_LANGUAGE_LIMIT:
+                messenger.bad_language = 0
+                ###
+                # This should be switched to addtempban sometime soon
+                zserv.zkick(messenger.number, "Bad language")
             else:
-                zserv.zsay("%s, this is a clean language server.  %d more violations and you will be kicked." % (p.name, BAD_LANGUAGE_LIMIT - p.bad_language))
+                s = "%s, this is a clean language server."
+                s += "  %d more violations and you will be kicked."
+                zserv.zsay(s % (p.name, BAD_LANGUAGE_LIMIT - p.bad_language))
