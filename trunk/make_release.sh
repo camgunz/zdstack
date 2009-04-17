0676bcdf35a8f9d5575b5a6ff2940ffa4ef6a061
Charles.Gunyon <Charles.Gunyon@36f78971-4549-0410-b7da-6d57fc7fc606>
2009-04-17 20:02:09 +0000

    Fuxing around with sphinx
    git-svn-id: http://zdstack.googlecode.com/svn@359 36f78971-4549-0410-b7da-6d57fc7fc606

--- trunk/make_release.sh
+++ trunk/make_release.sh
@@ -7,26 +7,42 @@ if [ ! $1 ]; then
   exit 1
 fi
 
-RELEASE_DIR="ZDStack-$1"
+RELEASE_DIR="`pwd`/ZDStack-$1"
 
+echo "=== Initializing release environment"
 rm -f $RELEASE_DIR.tar.bz2
 rm -f *.tar.bz2
 chmod 777 $RELEASE_DIR -R 2> /dev/null; rm -rf $RELEASE_DIR
 mkdir -p $RELEASE_DIR/bin $RELEASE_DIR/doc/pydocs $RELEASE_DIR/ZDStack
 
+echo "=== Copying executables"
 cp bin/zdstack bin/zservctl bin/zdrpc bin/zdsweb $RELEASE_DIR/bin/
+###
+# CURDIR=`pwd`
+# cd doc/pydocs/
+# for x in `find ../../ZDStack/ -name '*.py'`; do
+#   pydoc -w $x
+# done
+###
+# sphinx-build -b html /home/cagunyon/Desktop/Code/zdstack/trunk/ZDStack /home/cagunyon/Desktop/Code/zdstack/trunk/doc/pydocs/_build
+echo "=== Generating documentation"
+sphinx-build -b html /home/cagunyon/Desktop/Code/zdstack/trunk/ZDStack /home/cagunyon/Desktop/Code/zdstack/trunk/doc/pydocs
+echo "=== Copying documentation"
+cp doc/pydocs/*  $RELEASE_DIR/doc/pydocs/ 2> /dev/null
+cp doc/zdstack-example.ini $RELEASE_DIR/doc/
+echo "=== Copying module files"
 CURDIR=`pwd`
-cd doc/pydocs/
-for x in `find ../../ZDStack/ -name '*.py'`; do
-  pydoc -w $x
+cd ZDStack
+for x in *.py; do
+  if [ "$x" != 'conf.py' ]; then
+    cp "$x" "$RELEASE_DIR/ZDStack/"
+  fi
 done
 cd "$CURDIR"
-cp doc/pydocs/*.html  $RELEASE_DIR/doc/pydocs/
-cp doc/zdstack-example.ini $RELEASE_DIR/doc/
-cp ZDStack/*.py $RELEASE_DIR/ZDStack/
-cp * $RELEASE_DIR/ 2> /dev/null
-rm -f $RELEASE_DIR/make_release.sh
+echo "=== Copying distribution files"
+cp BUGS CHANGES INSTALL LICENSE README TODO setup.py $RELEASE_DIR/ 2> /dev/null
 
+echo "=== Setting permissions"
 chmod 640 $RELEASE_DIR/doc/pydocs/*.html
 chmod 640 $RELEASE_DIR/doc/zdstack-example.ini
 chmod 640 $RELEASE_DIR/ZDStack/*.py
@@ -38,7 +54,8 @@ chmod 750 $RELEASE_DIR/bin
 chmod 750 $RELEASE_DIR/doc
 chmod 750 $RELEASE_DIR/doc/pydocs
 chmod 750 $RELEASE_DIR/ZDStack
-chmod 750 $RELEASE_DIR/bin/* $RELEASE_DIR/setup.py
+chmod 750 $RELEASE_DIR/bin/*
+chmod 750 $RELEASE_DIR/setup.py
 
 tar cjf $RELEASE_DIR.tar.bz2 $RELEASE_DIR
 chmod 777 $RELEASE_DIR -R 2> /dev/null; rm -rf $RELEASE_DIR
